89603646cb95f57c1458369451f86ffbd967bdc1
BTC415 <alenschmitt415@gmail.com>
2021-03-07 15:56:56 +0100

    added footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,5 @@
+const Footer: React.FC = () => {
+  return <footer>&copy; Copyright 2021, Shoply Inc.</footer>;
+};
+
+export default Footer;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,13 @@
 //importing components
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 //main layout component
 const MainLayout: React.FC = ({ children }) => {
   return (
     <div className="main__layout">
       <Header />
-      <div className="wrapper">{children}</div>
+      <main className="wrapper">{children}</main>
+      <Footer />
     </div>
   );
 };
