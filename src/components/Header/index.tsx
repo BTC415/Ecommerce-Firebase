ec3a8912f0de1921cb205f6baa9427f78a858f32
BTC415 <alenschmitt415@gmail.com>
2021-03-07 15:52:48 +0100

    added registration links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,22 @@
+import { Link } from 'react-router-dom';
+
 const Header: React.FC = () => {
   return (
     <header>
       <div className="container">
         <div className="logo__container">
-          <h1>Shoply</h1>
+          <Link to="/" className="logo__link">
+            <h1>Shoply</h1>
+          </Link>
+        </div>
+        <div className="registration">
+          <ul className="registration__list">
+            <li>
+              <Link to="/registration" className="registration__link">
+                Register
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </header>
