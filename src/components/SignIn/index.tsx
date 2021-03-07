70c6acfdff308e63bb620cc327817447cd7ec413
BTC415 <alenschmitt415@gmail.com>
2021-03-07 18:58:04 +0100

    cleanup

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -3,7 +3,7 @@ import Button from '../Forms/Button';
 //importing google provider function
 import { signInWithGoogle } from '../../firebase/utils';
 //sign in component
-const SignIn = () => {
+const SignIn: React.FC = () => {
   //submit handler
   const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
