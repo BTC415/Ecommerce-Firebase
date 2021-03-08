342289b1b73830c809d6e6aac3039404e4a2815d
BTC415 <alenschmitt415@gmail.com>
2021-03-08 16:50:26 +0100

    adding sign up formsù

---
+++ src/components/Forms/FormInput/index.tsx
@@ -0,0 +1,24 @@
+//input props
+interface FormInputProps {
+  label?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  type: string;
+  value: string;
+  placeholder: string;
+  name: string;
+}
+//form input component
+const FormInput: React.FC<FormInputProps> = ({
+  onChange,
+  label,
+  ...otherProps
+}) => {
+  return (
+    <div className="form__input">
+      {label && <label>{label}</label>}
+      <input className="form__input" {...otherProps} onChange={onChange} />
+    </div>
+  );
+};
+
+export default FormInput;
--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -1,12 +1,44 @@
+//importing hooks
+import { useState } from 'react';
 //importing components
 import Button from '../Forms/Button';
+import FormInput from '../Forms/FormInput';
+//State interface
+interface FormElementsState {
+  displayName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
 //sign up component
 const SignUp: React.FC = () => {
+  //local state
+  const [formElements, setFormElements] = useState<FormElementsState>({
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  //on change handler
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormElements({ ...formElements, displayName: e.target.value });
+  };
+  //on submit handler
+  const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
   return (
     <div className="signup">
       <div className="container">
         <h2>Sign</h2>
-        <form>
+        <form onSubmit={onSubmitHandler}>
+          <FormInput
+            onChange={onChangeHandler}
+            type="text"
+            name="displayName"
+            placeholder="Full Name"
+            value={formElements.displayName}
+          />
           <Button>Sign In With Google</Button>
         </form>
       </div>
