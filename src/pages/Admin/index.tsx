4344e16e9d9513bafa7dbd61762df8c2200a0556
BTC415 <alenschmitt415@gmail.com>
2021-03-16 18:09:50 +0100

    adding a modal

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,5 +1,17 @@
+//importing components
+import Button from '../../components/Forms/Button';
+//admin component
 const Admin = () => {
-  return <div>Admin</div>;
+  return (
+    <div className="admin">
+      <div className="call__to__actions">
+        <ul>
+          <li>
+            <Button>Add new product</Button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
-
 export default Admin;
