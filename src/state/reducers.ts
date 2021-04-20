70bde2894c787fb0a575a296966620cd5da1e89c
BTC415 <alenschmitt415@gmail.com>
2021-04-21 00:06:38 +0200

    wired reducer

--- src/state/reducers.ts
+++ src/state/reducers.ts
@@ -1,11 +1,13 @@
 //importing reducers
 import { combineReducers } from 'redux';
+import cartReducer from './Cart/cart.reducer';
 import productsReducer from './Products/products.reducer';
 import userReducer from './User/user.reducer';
 //reducers
 const reducers = combineReducers({
   user: userReducer,
   productsData: productsReducer,
+  cart: cartReducer,
 });
 export default reducers;
 //ensuring that typescript knows the type of our state
