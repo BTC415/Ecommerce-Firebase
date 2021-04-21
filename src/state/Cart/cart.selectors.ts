c85a5166bc0228b753aca3dd17b1e944abf7f507
BTC415 <alenschmitt415@gmail.com>
2021-04-21 17:14:59 +0200

    adding a custom selector

--- src/state/Cart/cart.selectors.ts
+++ src/state/Cart/cart.selectors.ts
@@ -4,3 +4,16 @@ import { createSelector } from 'reselect';
 import { RootState } from '../reducers';
 //Selectors
 const selectCartData = (state: RootState) => state.cart;
+
+const selectCartItems = createSelector(
+  [selectCartData],
+  cart => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce((qty, cartItem) => {
+      return qty + cartItem.quantity;
+    }, 0)
+);
