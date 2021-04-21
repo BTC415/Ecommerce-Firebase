0311e889c883876459568c996637b5b7ef897337
BTC415 <alenschmitt415@gmail.com>
2021-04-21 16:15:37 +0200

    handling add to cart

--- src/components/Products/Product/index.tsx
+++ src/components/Products/Product/index.tsx
@@ -1,18 +1,19 @@
 //importing hooks
-import { useCartActions, useTypedSelector } from '../../../hooks';
-//importing router utils
+import { useCartActions } from '../../../hooks';
+//importing utils
 import { Link } from 'react-router-dom';
+//importing types
+import { ProductData } from '../../../state';
 //importing components
 import Button from '../../Forms/Button';
 //props interface
 interface ProductProps {
-  name: string;
-  price: number;
-  thumbnail: string;
-  documentId?: string;
+  product: ProductData;
 }
 //product component
-const Product: React.FC<ProductProps> = product => {
+const Product: React.FC<ProductProps> = ({ product }) => {
+  //redux actions
+  const { addProduct } = useCartActions();
   //destructuring
   const { name, price, thumbnail, documentId } = product;
   //type guards
@@ -22,7 +23,9 @@ const Product: React.FC<ProductProps> = product => {
     type = 'button',
   }
   //on click handler
-  const handleAddToCart = () => {};
+  const handleAddToCart = (product: ProductData) => {
+    addProduct(product);
+  };
   return (
     <div className="product__item">
       <div className="img__container">
@@ -37,7 +40,9 @@ const Product: React.FC<ProductProps> = product => {
         <div className="product__price">${price}</div>
       </div>
       <div className="add__to__cart">
-        <Button {...buttonConfig}>Add to cart</Button>
+        <Button {...buttonConfig} onClick={() => handleAddToCart(product)}>
+          Add to cart
+        </Button>
       </div>
     </div>
   );
