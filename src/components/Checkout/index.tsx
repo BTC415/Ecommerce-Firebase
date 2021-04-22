7ab00e765004ca0596fb7e2209e2c6b6fff075c9
BTC415 <alenschmitt415@gmail.com>
2021-04-22 23:10:11 +0200

    adding a checkout component

---
+++ src/components/Checkout/index.tsx
@@ -0,0 +1,9 @@
+const Checkout = () => {
+  return (
+    <div className="checkout">
+      <h1>Checkout</h1>
+    </div>
+  );
+};
+
+export default Checkout;
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,5 +1,12 @@
+//importing component
+import Checkout from '../../components/Checkout';
+//cart
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <div>
+      <Checkout />
+    </div>
+  );
 };
 
 export default Cart;
