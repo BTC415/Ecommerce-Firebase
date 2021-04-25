c8c588ebc8066651a3c32ee73705b5d846e3f092
BTC415 <alenschmitt415@gmail.com>
2021-04-25 20:41:38 +0200

    adding stripe to payments page

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,11 +1,16 @@
+//importing stripe utils
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 //importing components
 import PaymentDetails from '../../components/PaymentDetails';
+//stripe init
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_API_KEY!);
 //payment
 const Payment = () => {
   return (
-    <div>
+    <Elements stripe={stripePromise}>
       <PaymentDetails />
-    </div>
+    </Elements>
   );
 };
 
