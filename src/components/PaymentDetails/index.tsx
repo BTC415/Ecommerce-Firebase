62276b26c4eb5c34804c16c6007f1f1a83f7b4b0
BTC415 <alenschmitt415@gmail.com>
2021-04-25 16:48:00 +0200

    adding payment form details

--- src/components/PaymentDetails/index.tsx
+++ src/components/PaymentDetails/index.tsx
@@ -1,6 +1,40 @@
+//importing components
+import FormInput from '../Forms/FormInput';
+import Button from '../Forms/Button';
 //payment details
 const PaymentDetails = () => {
-  return <div className="payment__details"></div>;
+  //on submit handler
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="payment__details">
+      <form onSubmit={handleFormSubmit}>
+        <div className="group">
+          <h2>Shipping Address</h2>
+          <FormInput placeholder="Recipient Name" type="text" />
+          <FormInput placeholder="Line 1" type="text" />
+          <FormInput placeholder="Line 2" type="text" />
+          <FormInput placeholder="City" type="text" />
+          <FormInput placeholder="State" type="text" />
+          <FormInput placeholder="Postal Code" type="text" />
+        </div>
+        <div className="group">
+          <h2>Billing Address</h2>
+          <FormInput placeholder="Name on Card" type="text" />
+          <FormInput placeholder="Line 1" type="text" />
+          <FormInput placeholder="Line 2" type="text" />
+          <FormInput placeholder="City" type="text" />
+          <FormInput placeholder="State" type="text" />
+          <FormInput placeholder="Postal Code" type="text" />
+        </div>
+        <div className="group">
+          <h2>Card Details</h2>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default PaymentDetails;
